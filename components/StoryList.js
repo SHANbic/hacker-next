22011df11b953b6d17baba11ae1092c5bad37598
Pierre Lambert <pierre.lam91@gmail.com>
2019-06-20 22:37:02 +0200

    Updated StoryList comp to display more datas

--- components/StoryList.js
+++ components/StoryList.js
@@ -1,6 +1,19 @@
-const StoryList = ({ stories }) => {
-  return stories.map(story => {
-    return <h3 key={story.id}>{story.title}</h3>;
-  });
-};
+import Link from 'next/link';
+const StoryList = ({ stories }) => (
+  <div className="story-list">
+    {stories.map(story => (
+      <div className="story" key={story.id}>
+        <h2 className="story-title">
+          <a href={story.url}>{story.title}</a>
+        </h2>
+        <div className="story-details">
+          <span>{story.points || 0} points</span>
+          <Link href={`/story?id=${story.id}`}>
+            <a>{story.comments_count || 0} comments</a>
+          </Link>
+        </div>
+      </div>
+    ))}
+  </div>
+);
 export default StoryList;
